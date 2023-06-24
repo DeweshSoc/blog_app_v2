e59485cbdb33569d0ee7e4050662521b87dab264
Dewesh Jha <60105121+DeweshSoc@users.noreply.github.com>
2023-06-24 20:47:22 +0530

    update

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
-const express = require('express'); // express package exposes a lot of things as a function, we just store that function into the express constant
-
+const express = require("express"); // express package exposes a lot of things as a function, we just store that function into the express constant
 const app = express(); // express() returns an express app
 
+app.use(express.urlencoded());
 
 
-app.get('/',function(req,res){
-  console.log(req);
-  res.send('Hello!');  
-})
+const blogs = []; //array to store the blogs --> an array of objects
 
-
-
-
-
-
-app.listen(3000,function (){
-    console.log("server at 3000");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
+
+
+app.post("/send", function (req, res) {
+
+  const authorName = req.body.author; 
+  const blog = req.body.blog;
+
+
+  blogs.push({
+    author: authorName,
+    blogContent: blog,
+  });
+
+  console.log(blogs);
+
+  res.send(blogs);
+
+
+});
+
+app.listen(3000, function () {
+  console.log("server at 3000");
+});
+
+/*
+want something from server -> make a GET request
+want to give somthing to the server -> make a POST request
+
+
+*/
+
+
